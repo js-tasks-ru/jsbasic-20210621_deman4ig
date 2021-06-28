b9ec0b81e1306f70d98df6d256ef488fbe4954f7
Dmitry Fomin <dfomin@Dmitrys-MacBook-Pro.local>
2021-06-28 12:34:00 -0400

    Update tasks #4 #5 with the suggestions:
    Task #4 - replcase let with const in for clause
    Task #5 - return result w/o a var; remove brackets from ternary clause;

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,7 +1,7 @@
 function checkSpam(str) {
   const spamList = ['1xBet', 'XXX'];
 
-  for (let word of spamList) {
+  for (const word of spamList) {
     if (str.toLowerCase().includes(word.toLowerCase())) {
       return true;
     }
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,5 +1,3 @@
 function truncate(str, maxlength) {
-  const result = (str.length <= maxlength) ? str : `${str.slice(0, maxlength - 1)}…`;
-
-  return result;
+  return str.length <= maxlength ? str : `${str.slice(0, maxlength - 1)}…`;
 }
