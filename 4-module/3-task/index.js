77140f9e3ade938fb7c559cbdbd36ee572fe7042
Dmitry Fomin <dfomin@Dmitrys-MacBook-Pro.local>
2021-07-10 11:21:54 -0400

    Add solutions for all 3 tasks for lesson 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,25 @@
 function highlight(table) {
-  // ваш код...
+
+  for (const row of table.querySelector('tbody').rows) {
+    for (const cell of row.cells) {
+      if (cell.cellIndex == 1 && Number(cell.innerText) < 18) {
+        cell.parentElement.style.textDecoration = 'line-through';
+        continue;
+      }
+
+      if (cell.cellIndex == 2) {
+        cell.innerText == 'm' ? cell.parentElement.classList.add('male') : cell.parentElement.classList.add('female');
+      }
+
+      if (cell.cellIndex == 3) {
+        if (cell.dataset.available == undefined) {
+          cell.parentElement.hidden = true;
+        } else if (cell.dataset.available == 'true') {
+          cell.parentElement.classList.add('available');
+        } else {
+          cell.parentElement.classList.add('unavailable');
+        }
+      }
+    }
+  }
 }
